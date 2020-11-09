41239cbf3408c00c9bbc8f5e94ef9b3a2bdc58f5
LeandroDosSantosPereira <leandro.pereira@novaandradina.org>
2020-11-09 14:18:19 -0400

    add upload avatar

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ git_source(:github) do |repo_name|
 end
 
 gem 'jwt'
-
+gem 'carrierwave'
+gem 'carrierwave-base64'
 gem 'bcrypt'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2.1'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
-  has_one_attached :featured_image
+  has_secure_password 
+  mount_base64_uploader :photo, PhotoUploader
   validates :name,  :email, presence: true, uniqueness: true
 end
